ae0a7b22840796138546e23096c0c0a80f818d84
azu0nyan <nikgavgav@gmail.com>
2021-03-30 12:42:39 +0300

    input example

--- src/main/scala/examlples/HelloJME3.scala
+++ src/main/scala/examlples/HelloJME3.scala
@@ -1,10 +1,11 @@
+package examlples
+
 
 import com.jme3.app.SimpleApplication
 import com.jme3.material.Material
+import com.jme3.math.ColorRGBA
 import com.jme3.scene.Geometry
 import com.jme3.scene.shape.Box
-import com.jme3.math.ColorRGBA
-import com.jme3.system.AppSettings
 
 
 /** Sample 1 - how to get started with the most simple JME 3 application.
@@ -19,6 +20,7 @@ object HelloJME3 {
 }
 class HelloJME3 extends SimpleApplication {
   override def simpleInitApp(): Unit = {
+
     val b = new Box(1, 1, 1) // create cube shape
     val geom = new Geometry("Box", b) // create cube geometry from the shape
     val mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md") // create a simple material
